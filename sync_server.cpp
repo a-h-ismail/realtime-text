d554f29ab2020fc007842e4c847df90de5955b46
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-03-25 13:12:57 +0200

    Add a basic server loop.

--- sync_server.cpp
+++ sync_server.cpp
@@ -15,6 +15,40 @@
 
 using namespace std;
 
+void clients_cleanup(vector<Client *> &clients)
+{
+    vector<payload> commands;
+    while (1)
+    {
+        // Remove disconnected clients and collect all commands
+        for (int i = 0; i < clients.size(); ++i)
+        {
+            if (clients[i]->closed)
+            {
+                cout << "Connection To " << inet_ntoa(clients[i]->socket.sin_addr) << ":"
+                     << (uint16_t)clients[i]->socket.sin_port << " is closed" << endl;
+                delete clients[i];
+                clients.erase(clients.begin() + i);
+                break;
+            }
+            // Lock reception in the target client
+            clients[i]->lock_recv.lock();
+            // Retrieve received payloads
+            commands.insert(commands.end(), clients[i]->recv_commands.begin(), clients[i]->recv_commands.end());
+            // Clear the current list of commands from the client (to make room for new ones)
+            clients[i]->recv_commands.clear();
+            clients[i]->lock_recv.unlock();
+        }
+
+        for (int i = 0; i < clients.size(); ++i)
+            clients[i]->send_commands(commands);
+
+        commands.clear();
+        // Provide every client instance with the command list
+        usleep(50000);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     struct sockaddr_in client_socket, server_socket;
@@ -39,14 +73,17 @@ int main(int argc, char *argv[])
     Client *new_arrival;
     Openfile the_file("useful_text.txt");
     int client_size = sizeof(client_socket), client_descriptor;
-    uint16_t payload_size;
-    rt_command function;
+
+    thread transmitter(clients_cleanup, ref(clients));
+    transmitter.detach();
+
     while (1)
     {
         client_descriptor = accept(server_descriptor, (SA *)&client_socket, (socklen_t *)&client_size);
         new_arrival = new Client("dummy", client_socket, client_descriptor);
         clients.push_back(new_arrival);
         new_arrival->push_file(the_file);
+        new_arrival->start_sync();
     }
     return 0;
 }
