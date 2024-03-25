07c8ea270658145b5a0b8009b6318b72b0cea9e7
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-03-25 13:09:57 +0200

    Use a hash map for quick ID to line lookup.

--- file.h
+++ file.h
@@ -2,6 +2,7 @@
 #include <random>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 
 typedef struct file_node
 {
@@ -12,6 +13,9 @@ typedef struct file_node
 
 class Openfile
 {
+private:
+    std::unordered_map<int32_t, std::list<file_node>::iterator> id_to_line;
+
 public:
     std::ifstream fs;
     std::list<file_node> lines;
@@ -21,4 +25,12 @@ public:
     Openfile(const char *filename);
 
     void set_file(const char *filename);
+
+    int insert_str_at(int32_t line_id, int32_t column, std::string substr);
+
+    int break_line_at(int32_t line_id, int32_t column, std::string substr);
+
+    int add_line(int32_t line_id);
+
+    int remove_line(int32_t line_id);
 };
