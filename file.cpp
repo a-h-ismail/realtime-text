9d6cd8c7041c7756dea91aded40ae35fc2cdf03f
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-04-28 18:00:08 +0300

    Catch non existant line ID instead of crashing.

--- file.cpp
+++ file.cpp
@@ -46,63 +46,77 @@ void Openfile::save_file()
 
 int Openfile::insert_str_at(int32_t line_id, int32_t column, string substr)
 {
-    auto target_line = id_to_line[line_id];
-    if (column < target_line->data.size())
-        target_line->data.insert(column, substr);
-    else
-        target_line->data.append(substr);
-    return 0;
+    try
+    {
+        auto target_line = id_to_line.at(line_id);
+        if (column < target_line->data.size())
+            target_line->data.insert(column, substr);
+        else
+            target_line->data.append(substr);
+        return 0;
+    }
+    catch (out_of_range)
+    {
+        return 1;
+    }
 }
 
 int Openfile::remove_substr(int32_t line_id, int32_t column, int32_t count)
 {
-    auto target_line = id_to_line[line_id];
-    // Remove line break and merge with previous line
-    if (column == -1)
+    try
     {
-        // We can't delete the line before the first line
-        if (target_line == lines.begin())
-            return 0;
-        auto prev = target_line;
-        --prev;
-        // If the amount to delete is more than 1, we need to remove count-1 characters from the beginning
-        if (count > 1)
-            target_line->data.erase(0, count - 1);
+        auto target_line = id_to_line.at(line_id);
+        // Remove line break and merge with previous line
+        if (column == -1)
+        {
+            // We can't delete the line before the first line
+            if (target_line == lines.begin())
+                return 0;
+            auto prev = target_line;
+            --prev;
+            // If the amount to delete is more than 1, we need to remove count-1 characters from the beginning
+            if (count > 1)
+                target_line->data.erase(0, count - 1);
 
-        prev->data += target_line->data;
-        id_to_line.erase(target_line->line_id);
-        lines.erase(target_line);
+            prev->data += target_line->data;
+            id_to_line.erase(target_line->line_id);
+            lines.erase(target_line);
+        }
+        // Remove line break and merge with next line
+        else if (column + count > target_line->data.size())
+        {
+            if (target_line == lines.end())
+                return 0;
+
+            auto next = target_line;
+            ++next;
+            if (count > 1)
+                next->data.erase(0, count - 1);
+            target_line->data += next->data;
+            id_to_line.erase(next->line_id);
+            lines.erase(next);
+        }
+        else
+            target_line->data.erase(column, count);
+        return 0;
     }
-    // Remove line break and merge with next line
-    else if (column + count > target_line->data.size())
+    catch (out_of_range)
     {
-        if (target_line == lines.end())
-            return 0;
-
-        auto next = target_line;
-        ++next;
-        if (count > 1)
-            next->data.erase(0, count - 1);
-        target_line->data += next->data;
-        id_to_line.erase(next->line_id);
-        lines.erase(next);
+        return 1;
     }
-    else
-        target_line->data.erase(column, count);
-    return 0;
 }
 
 int Openfile::break_line_at(int32_t line_id, int32_t column, int32_t newline_id, std::string prefix)
 {
     try
     {
-        list<file_node>::iterator target_line = id_to_line.at(line_id);
+        auto target_line = id_to_line.at(line_id);
         file_node newline;
         // Generate the content of the new line below
         newline.data = prefix + target_line->data.substr(column);
         newline.line_id = newline_id;
         lines.insert(next(target_line), newline);
-        id_to_line[newline_id] = ++target_line;
+        id_to_line[newline_id] = next(target_line);
         // Remove the part of the line that was pushed below
         target_line->data.erase(column);
         return 0;
@@ -115,35 +129,56 @@ int Openfile::break_line_at(int32_t line_id, int32_t column, int32_t newline_id,
 
 int Openfile::add_line(int32_t after_id, int32_t with_id, string data)
 {
-    auto target_line = id_to_line[after_id];
-    file_node newline;
-    newline.data = data;
-    newline.line_id = with_id;
-    if (target_line == lines.end())
+    try
     {
-        lines.push_back(newline);
-        id_to_line[with_id] = lines.end();
+        auto target_line = id_to_line.at(after_id);
+        file_node newline;
+        newline.data = data;
+        newline.line_id = with_id;
+        if (target_line == lines.end())
+        {
+            lines.push_back(newline);
+            id_to_line[with_id] = lines.end();
+        }
+        else
+        {
+            ++target_line;
+            lines.insert(target_line, newline);
+            --target_line;
+            id_to_line[with_id] = target_line;
+        }
+        return 0;
     }
-    else
+    catch (out_of_range)
     {
-        ++target_line;
-        lines.insert(target_line, newline);
-        --target_line;
-        id_to_line[with_id] = target_line;
+        return 1;
     }
-    return 0;
 }
 
 int Openfile::remove_line(int32_t line_id)
 {
-    auto target_line = id_to_line[line_id];
-    lines.erase(target_line);
-    return 0;
+    try
+    {
+        auto target_line = id_to_line.at(line_id);
+        lines.erase(target_line);
+        return 0;
+    }
+    catch (out_of_range)
+    {
+        return 1;
+    }
 }
 
 int Openfile::replace_line(int32_t line_id, string new_data)
 {
-    auto target_line = id_to_line[line_id];
-    target_line->data = new_data;
-    return 0;
+    try
+    {
+        auto target_line = id_to_line.at(line_id);
+        target_line->data = new_data;
+        return 0;
+    }
+    catch (out_of_range)
+    {
+        return 1;
+    }
 }
